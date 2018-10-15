d8c0b9bed18b634f52124e9703e9a696df345cd6
Grixi <adam.tretera@educanet.cz>
2018-10-15 20:26:28 +0200

    js hodiny
    js hodiny

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function showTime() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  var time = h + ":" + m +":"+ s ;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+showTime();
