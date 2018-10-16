71712c7411afde3c89c89c04abda403a78c13eb4
PeanutButte7 <adam.barta007@gmail.com>
2018-10-16 14:43:21 +0200

    Hodiny 0.2

--- js/script.js
+++ js/script.js
@@ -2,16 +2,26 @@ function showTime() {
   var date = new Date();
   var h = date.getHours();
   var m = date.getMinutes();
-  var s = date.getSeconds();
+  var hA;
+  //var s = date.getSeconds();
 
   h = (h < 10) ? "0" + h : h;
+  hA = (hA < 10) ? "0" + h : h;
   m = (m < 10) ? "0" + m : m;
-  s = (s < 10) ? "0" + s : s;
+  //s = (s < 10) ? "0" + s : s;
 
-  var time = h + ":" + m +":"+ s ;
+  hA = h - 7;
+
+  var time = h + ":" + m;
+  var timeA = hA + ":" + m;
   document.getElementById("mojehodiny").innerText = time;
   document.getElementById("mojehodiny").textContent = time;
 
+  document.getElementById("hodinyA").innerText = timeA;
+  document.getElementById("hodinyA").textContent = timeA;
+
+
   setTimeout(showTime, 1000);
 }
+
 showTime();
