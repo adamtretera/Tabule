da49e0ab3b20fa82378509f6263d059a33b3f0df
Grixi <adam.tretera@educanet.cz>
2018-10-18 12:39:51 +0200

    Update index.html

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,108 @@
 function showTime() {
   var date = new Date();
-  var h = date.getHours();
-  var m = date.getMinutes();
-  var s = date.getSeconds();
+  var timeUTCMinutes = date.getUTCMinutes();
+  timeUTCMinutes = (timeUTCMinutes < 10) ? "0" + timeUTCMinutes : timeUTCMinutes;
+  var timeUTC =  date.getUTCHours() + ":" + timeUTCMinutes;
 
-  h = (h < 10) ? "0" + h : h;
-  m = (m < 10) ? "0" + m : m;
-  s = (s < 10) ? "0" + s : s;
+  //h = (h < 10) ? "0" + h : h;
 
-  var time = h + ":" + m +":"+ s ;
-  document.getElementById("mojehodiny").innerText = time;
-  document.getElementById("mojehodiny").textContent = time;
+  document.getElementById("hodinyUTC").innerText = timeUTC;
+  document.getElementById("hodinyUTC").textContent = timeUTC;
 
   setTimeout(showTime, 1000);
 }
+
+function showTimeCzech() {
+  var date = new Date();
+  date.setMinutes(date.getMinutes() + 120);
+
+  var timeCzechMinutes = date.getUTCMinutes();
+  var timeCzechHours = date.getUTCHours();
+
+  timeCzechMinutes = (timeCzechMinutes < 10) ? "0" + timeCzechMinutes : timeCzechMinutes;
+  timeCzechHours = (timeCzechHours < 10) ? "0" + timeCzechHours : timeCzechHours;
+
+  var timeCzech = timeCzechHours + ":" + timeCzechMinutes;
+
+  document.getElementById("mojehodiny").innerText = timeCzech;
+  document.getElementById("mojehodiny").textContent = timeCzech;
+
+  setTimeout(showTimeCzech, 1000);
+}
+
+function showTimeTokyo() {
+  var date = new Date();
+  date.setMinutes(date.getMinutes() + 540);
+
+  var timeTokyoMinutes = date.getUTCMinutes();
+  var timeTokyoHours = date.getUTCHours();
+
+  timeTokyoMinutes = (timeTokyoMinutes < 10) ? "0" + timeTokyoMinutes : timeTokyoMinutes;
+
+  var timeTokyo = timeTokyoHours + ":" + timeTokyoMinutes;
+
+  document.getElementById("hodinyT").innerText = timeTokyo;
+  document.getElementById("hodinyT").textContent = timeTokyo;
+
+  setTimeout(showTimeTokyo, 1000);
+}
+
+function showTimeUSA() {
+  var date = new Date();
+  date.setMinutes(date.getMinutes() - 240);
+
+  var timeUSAMinutes = date.getUTCMinutes();
+  var timeUSAHours = date.getUTCHours();
+
+  timeUSAMinutes = (timeUSAMinutes < 10) ? "0" + timeUSAMinutes : timeUSAMinutes;
+
+  var timeUSA = timeUSAHours + ":" + timeUSAMinutes;
+
+  document.getElementById("hodinyA").innerText = timeUSA;
+  document.getElementById("hodinyA").textContent = timeUSA;
+
+  setTimeout(showTimeUSA, 1000);
+}
+
+function showTimeLondon() {
+  var date = new Date();
+  date.setMinutes(date.getMinutes() + 180);
+
+  var timeLondonMinutes = date.getUTCMinutes();
+  var timeLondonHours = date.getUTCHours();
+
+  timeLondonMinutes = (timeLondonMinutes < 10) ? "0" + timeLondonMinutes : timeLondonMinutes;
+
+  var timeLondon = timeLondonHours + ":" + timeLondonMinutes;
+
+  document.getElementById("hodinyL").innerText = timeLondon;
+  document.getElementById("hodinyL").textContent = timeLondon;
+
+  setTimeout(showTimeLondon, 1000);
+}
+
+function countdown() {
+  var date = new Date();
+  date.setMinutes(date.getMinutes() + 120);
+
+  var timeCzechMinutes = date.getUTCMinutes();
+  var timeCzechHours = date.getUTCHours();
+
+  timeCzechMinutes = (timeCzechMinutes < 10) ? "0" + timeCzechMinutes : timeCzechMinutes;
+  timeCzechHours = (timeCzechHours < 10) ? "0" + timeCzechHours : timeCzechHours;
+
+  var timeCzech = timeCzechHours + ":" + timeCzechMinutes;
+
+  document.getElementById("countdownID").innerText = timeCzech;
+  document.getElementById("countdownID").textContent = timeCzech;
+
+  setTimeout(countdown, 1000);
+}
+
+
 showTime();
+showTimeCzech();
+showTimeTokyo();
+showTimeUSA();
+showTimeLondon();
+countdown();
