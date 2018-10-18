ecf25e2ddfd2bd7b8611f46d8b14de335d5d3b94
PeanutButte7 <adam.barta007@gmail.com>
2018-10-18 11:11:54 +0200

    JavaScript timer 0.01

--- js/script.js
+++ js/script.js
@@ -81,23 +81,29 @@ function showTimeLondon() {
   setTimeout(showTimeLondon, 1000);
 }
 
-function countdown() {
-  var date = new Date();
-  date.setMinutes(date.getMinutes() + 120);
+//Sets deadline
+var deadline = new Date("Jan 5, 2019 15:37:25").getTime();
 
-  var timeCzechMinutes = date.getUTCMinutes();
-  var timeCzechHours = date.getUTCHours();
+var x = setInterval(function() {
+  var now = new Date().getTime(); //Date now
+  var t = deadline - now; //Time remaining
 
-  timeCzechMinutes = (timeCzechMinutes < 10) ? "0" + timeCzechMinutes : timeCzechMinutes;
-  timeCzechHours = (timeCzechHours < 10) ? "0" + timeCzechHours : timeCzechHours;
+  //Rounds the values and splits them
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((t % (1000 * 60)) / 1000);
 
-  var timeCzech = timeCzechHours + ":" + timeCzechMinutes;
+  //Output
+  document.getElementById("countdownID").innerHTML = days + "d "+ hours + "h " + minutes + "m " + seconds + "s ";
 
-  document.getElementById("countdownID").innerText = timeCzech;
-  document.getElementById("countdownID").textContent = timeCzech;
+  //What happens when the timer ends
+  if (t < 0) {
+      clearInterval(x);
+      document.getElementById("countdownID").innerHTML = "EXPIRED";
+  }
+}, 1000);
 
-  setTimeout(countdown, 1000);
-}
 
 
 showTime();
