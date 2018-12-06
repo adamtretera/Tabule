725538c7449e405920cb98bff85a347f0d18202f
PeanutButte7 <adam.barta007@gmail.com>
2018-12-06 16:04:30 +0100

    Materilaze 0.2

--- js/weather.js
+++ js/weather.js
@@ -5,8 +5,8 @@ function repeat(){
       var work = JSON.parse(request.responseText); //prevadi vraceny JSON na string
 
       document.getElementById("iconID").src = "Icons/Weather Icons/SVG/" + work.weather[0].icon + ".svg";
-      document.getElementById("temperatureID").innerHTML = work.main.temp + "°C";
-      document.getElementById("windID").innerHTML = work.wind.speed + 'm/s';
+      document.getElementById("temperatureID").innerHTML = (work.main.temp).toFixed(1)  + "°C";
+      document.getElementById("windID").innerHTML = (work.wind.speed).toFixed(1)  + 'm/s';
       document.getElementById("humidityID").innerHTML = work.main.humidity + '%';
     }
   };
@@ -35,29 +35,29 @@ function repeatDaily(){
       //za 3h
       document.getElementById("hours1").innerHTML = moment(work.list[1].dt_txt).format('HH') + ':00';
       document.getElementById("iconID1").src = "Icons/Weather Icons/SVG/" + work.list[1].weather[0].icon + ".svg";
-      document.getElementById("tempID1").innerHTML = work.list[1].main.temp + "°C";
-      document.getElementById("windID1").innerHTML = work.list[1].wind.speed + 'm/s';
+      document.getElementById("tempID1").innerHTML = (work.list[1].main.temp).toFixed(1) + "°C";
+      document.getElementById("windID1").innerHTML = (work.list[1].wind.speed).toFixed(1) + 'm/s';
       document.getElementById("humidityID1").innerHTML = work.list[1].main.humidity + '%';
       //za 6h
       document.getElementById("hours2").innerHTML = moment(work.list[2].dt_txt).format('HH') + ':00';
       document.getElementById("iconID2").src = "Icons/Weather Icons/SVG/" + work.list[2].weather[0].icon + ".svg";
-      document.getElementById("tempID2").innerHTML = work.list[2].main.temp + "°C";
-      document.getElementById("windID2").innerHTML = work.list[2].wind.speed + 'm/s';
+      document.getElementById("tempID2").innerHTML = (work.list[2].main.temp).toFixed(1) + "°C";
+      document.getElementById("windID2").innerHTML = (work.list[2].wind.speed).toFixed(1) + 'm/s';
       document.getElementById("humidityID2").innerHTML = work.list[2].main.humidity + '%';
       //zitra - 8 (12:00)
       document.getElementById("iconID3").src = "Icons/Weather Icons/SVG/" + work.list[8].weather[0].icon + ".svg";
-      document.getElementById("tempID3").innerHTML = work.list[8].main.temp + "°C";
-      document.getElementById("windID3").innerHTML = work.list[8].wind.speed + 'm/s';
+      document.getElementById("tempID3").innerHTML = (work.list[8].main.temp).toFixed(1) + "°C";
+      document.getElementById("windID3").innerHTML = (work.list[8].wind.speed).toFixed(1) + 'm/s';
       document.getElementById("humidityID3").innerHTML = work.list[8].main.humidity + '%';
       //pozitri - 16 (12:00)
       document.getElementById("iconID4").src = "Icons/Weather Icons/SVG/" + work.list[16].weather[0].icon + ".svg";
-      document.getElementById("tempID4").innerHTML = work.list[16].main.temp + "°C";
-      document.getElementById("windID4").innerHTML = work.list[16].wind.speed + 'm/s';
+      document.getElementById("tempID4").innerHTML = (work.list[16].main.temp).toFixed(1) + "°C";
+      document.getElementById("windID4").innerHTML = (work.list[16].wind.speed).toFixed(1) + 'm/s';
       document.getElementById("humidityID4").innerHTML = work.list[16].main.humidity + '%';
       //po..pozitri - 24 (12:00)
       document.getElementById("iconID5").src = "Icons/Weather Icons/SVG/" + work.list[24].weather[0].icon + ".svg";
-      document.getElementById("tempID5").innerHTML = work.list[24].main.temp + "°C";
-      document.getElementById("windID5").innerHTML = work.list[24].wind.speed + 'm/s';
+      document.getElementById("tempID5").innerHTML = (work.list[24].main.temp).toFixed(1) + "°C";
+      document.getElementById("windID5").innerHTML = (work.list[24].wind.speed).toFixed(1) + 'm/s';
       document.getElementById("humidityID5").innerHTML = work.list[24].main.humidity + '%';
     }
   };
