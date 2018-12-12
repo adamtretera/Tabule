67762581f9fb03010d05049e039e1718d857c7ff
PeanutButte7 <adam.barta007@gmail.com>
2018-12-12 19:40:56 +0100

    Nehrab do toho <3

--- js/autobusy.js
+++ js/autobusy.js
@@ -8,16 +8,37 @@ function bus() {
       var dayOfTheWeek = date.getDay(); //pocita se od nuly (0-6)
       var currentTime = date.getHours() + ":" + date.getMinutes();
 
-      console.log(dayOfTheWeek);
-      console.log(currentTime);
-      var checkTime = "work.tyden_SmerChodov[0].cas";
+      if(dayOfTheWeek <= 4){ //TYDEN
+        var checkTime = "work.tyden_SmerChodov[0].cas";
 
-      for(var i = 0; currentTime > eval(checkTime); i++){
-        console.log("pokus c." + i)
-        checkTime = "work.tyden_SmerChodov[" + i + "].cas";
-        if(eval(checkTime) >= currentTime){
-          var firstBus = "work.tyden_SmerChodov[" + i + "].cas";
-          console.log("Nejblizsi autobus " + eval(firstBus));
+        for(var i = 0; currentTime > eval(checkTime); i++){
+          checkTime = "work.tyden_SmerChodov[" + i + "].cas";
+          if(eval(checkTime) >= currentTime){
+            var firstBus = "work.tyden_SmerChodov[" + i + "].cas";
+            var firstBusNumber = "work.tyden_SmerChodov[" + i + "].cislo";
+            console.log("Nejblizsi autobus " + eval(firstBus));
+            console.log("Cislo autobusu " + eval(firstBusNumber));
+          }
+        }
+      } else if(dayOfTheWeek == 5){ //SOBOTA
+        var checkTime = "work.sobota_SmerChodov[0].cas";
+
+        for(var i = 0; currentTime > eval(checkTime); i++){
+          checkTime = "work.sobota_Chodov[" + i + "].cas";
+          if(eval(checkTime) >= currentTime){
+            var firstBus = "work.sobota_SmerChodov[" + i + "].cas";
+            console.log("Nejblizsi autobus " + eval(firstBus));
+          }
+        }
+      } else if(dayOfTheWeek == 6){ //NEDELE
+        var checkTime = "work.nedele_SmerChodov[0].cas";
+
+        for(var i = 0; currentTime > eval(checkTime); i++){
+          checkTime = "work.nedele_Chodov[" + i + "].cas";
+          if(eval(checkTime) >= currentTime){
+            var firstBus = "work.nedele_SmerChodov[" + i + "].cas";
+            console.log("Nejblizsi autobus " + eval(firstBus));
+          }
         }
       }
     }
@@ -25,6 +46,8 @@ function bus() {
 
   request.open('GET', 'js/json/busy.json', true); //adresa na kterou se ma pripojit
   request.send(); //odesle request
+
+  setTimeout(bus, 10*1000);
 }
 
 bus();
